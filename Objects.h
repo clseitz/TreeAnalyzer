406bb29f4e0a57e475ca708144cfa8af48f5cad9
Artur Lobanov <artur.lobanov@desy.de>
2014-10-17 14:14:53 +0200

    Extending object cuts

--- Objects.h
+++ Objects.h
@@ -21,13 +21,13 @@
  */
 
 void GetGoodLeptons(EasyChain * tree);
-void GetGoodJets(EasyChain * tree);
+void GetJets(EasyChain * tree);
+void GetLeptons(EasyChain * tree);
 
 // to be written
 /*
 void GetElectrons(EasyChain * tree);
 void GetMuons(EasyChain * tree);
-void GetLeptons(EasyChain * tree);
 
 void GetGoodElectrons(EasyChain * tree);
 void GetGoodMuons(EasyChain * tree);
@@ -37,16 +37,25 @@ void GetJets(EasyChain * tree);
 
 // global object variables
 extern std::vector<TLorentzVector> goodJet;
+extern std::vector<TLorentzVector> goodBJet;
 extern std::vector<TLorentzVector> goodLep;
 extern std::vector<TLorentzVector> goodEl;
 extern std::vector<TLorentzVector> goodMu;
 extern std::vector<TLorentzVector> vetoLep;
+extern std::vector<TLorentzVector> vetoEl;
+extern std::vector<TLorentzVector> vetoMu;
+
 
 int nLepGood;
 int nMuGood;
 int nElGood;
 
+int nLepVeto;
+int nElVeto;
+int nMuVeto;
+
 int nJetGood;
+int nBJetGood;
 
 Float_t HT40;
 Float_t ST;
