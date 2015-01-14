689668268de6c9e10ae5fde22256733c3df27322
safarzad <safarzad@cern.ch>
2015-01-14 15:19:52 +0100

    add T1ttbbWW samples

--- CMGToolAna/python/DESYsamples.py
+++ CMGToolAna/python/DESYsamples.py
@@ -7,7 +7,7 @@ def scale(fac,list):
 # scenarios and samples
 scenarios = ['MC','data']
 #SAMPLES = ['WJets','TTbar','TTbar_DiLep','TTbar_SinLep','T1tttt_1500_100','T1tttt_1200_800','T1tttt_800_450','T1tttt_1300_100','T1qqqq_1400_15']
-SAMPLES = ['WJets','TTbar','TTbar_DiLep','TTbar_SinLep','DYJets','QCD','SingleTop','TTH','TTW','TTZ','T1tttt_1500_100','T1tttt_1200_800','T5tttt_1000_280','T5tttt_1300_280','T5tttt_1000_285','T5tttt_1300_285']
+SAMPLES = ['WJets','TTbar','TTbar_DiLep','TTbar_SinLep','DYJets','QCD','SingleTop','TTH','TTW','TTZ','T1tttt_1500_100','T1tttt_1200_800','T5tttt_1000_280','T5tttt_1300_280','T5tttt_1000_285','T5tttt_1300_285','T1ttbbWW_1300_290','T1ttbbWW_1300_295','T1ttbbWW_1000_715','T1ttbbWW_1000_720']
 treename = 'treeProducerSusySingleLepton/'
 base = '/nfs/dust/cms/group/susy-desy/Run2/MC/CMGtuples/Phys14_v1/'
 Lumi = 4 #given in fb^-1
@@ -141,6 +141,31 @@ sample = 'T5tttt_1000_285'
 dirsHT[sample] = ['/']
 xsec_lumi[sample] = [0.325388*Lumi]
 inDir['MC'][sample] = base+'T5ttttDeg_mGo1000_mStop300_mCh285_mChi280/'
+
+
+sample = 'T1ttbbWW_1000_715'
+dirsHT[sample] = ['/']
+xsec_lumi[sample] = [0.325388*Lumi]
+inDir['MC'][sample] = base+'T1ttbbWW_mGo1000_mCh725_mChi715/'
+
+
+sample = 'T1ttbbWW_1000_720'
+dirsHT[sample] = ['/']
+xsec_lumi[sample] = [0.325388*Lumi]
+inDir['MC'][sample] = base+'T1ttbbWW_mGo1000_mCh725_mChi720/'
+
+
+sample = 'T1ttbbWW_1300_290'
+dirsHT[sample] = ['/']
+xsec_lumi[sample] = [0.044*Lumi]
+inDir['MC'][sample] = base+'T1ttbbWW_mGo1300_mCh300_mChi290/'
+
+sample = 'T1ttbbWW_1300_295'
+dirsHT[sample] = ['/']
+xsec_lumi[sample] = [0.044*Lumi]
+inDir['MC'][sample] = base+'T1ttbbWW_mGo1300_mCh300_mChi295/'
+
+
 
 '''
 sample = 'T1tttt_800_450'
